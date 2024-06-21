e9626687da72adc648d5e2354ccb3c298d59d081
Hoyama1122 <ffork859@gmail.com>
2024-06-21 14:38:32 +0700

    Success ReportStock Page

--- api/controllers/StockController.js
+++ api/controllers/StockController.js
@@ -3,7 +3,6 @@ const app = express();
 const service = require("./Service");
 
 const StockModel = require("../models/StockModel");
-const { where } = require("sequelize");
 
 app.post("/stock/save", service.isLogin, async (req, res) => {
   try {
@@ -25,7 +24,6 @@ app.post("/stock/save", service.isLogin, async (req, res) => {
 app.get("/stock/list", service.isLogin, async (req, res) => {
   try {
     const userId = service.getMemberId(req);
-
     const ProductModel = require("../models/ProductModel");
     StockModel.belongsTo(ProductModel);
 
@@ -62,6 +60,7 @@ app.delete("/stock/delete/:id", service.isLogin, async (req, res) => {
 app.get("/stock/report", service.isLogin, async (req, res) => {
   try {
     const ProductModel = require("../models/ProductModel");
+    const StockModel = require("../models/StockModel");
     const BillSaleDetailModel = require("../models/BillSaleDetailModel");
 
     const userId = service.getMemberId(req);
@@ -69,20 +68,30 @@ app.get("/stock/report", service.isLogin, async (req, res) => {
     ProductModel.hasMany(StockModel);
     ProductModel.hasMany(BillSaleDetailModel);
 
-    let arr = [];
+    StockModel.belongsTo(ProductModel);
+    BillSaleDetailModel.belongsTo(ProductModel);
+
     const results = await ProductModel.findAll({
       include: [
         {
           model: StockModel,
+          include: {
+            model: ProductModel,
+          },
         },
         {
           model: BillSaleDetailModel,
+          include: {
+            model: ProductModel,
+          },
         },
       ],
       where: {
         userId: userId,
       },
     });
+
+    let arr = [];
 
     for (let i = 0; i < results.length; i++) {
       const result = results[i];
@@ -92,18 +101,27 @@ app.get("/stock/report", service.isLogin, async (req, res) => {
       let stockIn = 0;
       let stockOut = 0;
 
+      const stockInDetails = [];
+      const stockOutDetails = [];
+
       for (let j = 0; j < stocks.length; j++) {
         const item = stocks[j];
         stockIn += parseInt(item.qty);
+        stockInDetails.push(item);
       }
+
       for (let j = 0; j < billsaledetails.length; j++) {
         const item = billsaledetails[j];
         stockOut += parseInt(item.qty);
+        stockOutDetails.push(item);
       }
+
       arr.push({
         product: result,
         stockIn: stockIn,
         stockOut: stockOut,
+        stockInDetails: stockInDetails,
+        stockOutDetails: stockOutDetails,
       });
     }
 
@@ -112,5 +130,6 @@ app.get("/stock/report", service.isLogin, async (req, res) => {
     res.status(500).send({ message: e.message });
   }
 });
+
 
 module.exports = app;
--- web/app/src/page/ReportStock.jsx
+++ web/app/src/page/ReportStock.jsx
@@ -4,9 +4,12 @@ import swal from "sweetalert2";
 import axios from "axios";
 import config from "../config";
 import Modal from "../components/Modal";
+import * as dayjs from "dayjs";
 
 export default function ReportStock() {
   const [stocks, setStocks] = useState([]);
+  const [currentStock, setCurrentStock] = useState({});
+  const [modalType, setModalType] = useState(""); // to track which modal to show
 
   useEffect(() => {
     fetchData();
@@ -26,6 +29,11 @@ export default function ReportStock() {
     } catch (e) {
       ShowError(e.message);
     }
+  };
+
+  const selectStock = (item, type) => {
+    setCurrentStock(item);
+    setModalType(type);
   };
 
   const ShowError = (message) => {
@@ -54,7 +62,6 @@ export default function ReportStock() {
                   <td>คงเหลือ</td>
                 </tr>
               </thead>
-
               <tbody className="text-center">
                 {stocks.map((item, index) => (
                   <tr key={index}>
@@ -62,6 +69,7 @@ export default function ReportStock() {
                     <td>{item.product.name}</td>
                     <td>
                       <a
+                        onClick={() => selectStock(item, "stockIn")}
                         className="text-success"
                         data-bs-toggle="modal"
                         data-bs-target="#modalReport"
@@ -69,7 +77,16 @@ export default function ReportStock() {
                         {item.stockIn}
                       </a>
                     </td>
-                    <td className="text-danger">{item.stockOut}</td>
+                    <td className="text-danger">
+                      <a
+                        onClick={() => selectStock(item, "stockOut")}
+                        className="text-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modalReport"
+                      >
+                        {item.stockOut}
+                      </a>
+                    </td>
                     <td className="text-primary fw-bold h5">
                       {item.stockIn - item.stockOut}
                     </td>
@@ -81,9 +98,48 @@ export default function ReportStock() {
         </div>
       </Template>
 
-      <Modal id="modalReport" title="รายงาน Stock" modalSize="modal-lg">
+      <Modal id="modalReport" title={`รายงาน ${modalType === 'stockIn' ? 'StockIn' : 'StockOut'}`} modalSize="modal-lg">
         <table className="table table-bordered table-striped table-hover mt-3">
-          <thead className="table-white text-center"></thead>
+          <thead className="table-white text-center">
+            <tr>
+              <th>Barcode</th>
+              <th>รายการ</th>
+              <th>วันที่</th>
+              <th>จำนวน</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {currentStock && currentStock[`${modalType}Details`] && currentStock[`${modalType}Details`].length > 0 ? (
+              currentStock[`${modalType}Details`].map((item, index) => (
+                <tr key={index}>
+                  <td>{item.product.barcode}</td>
+                  <td>{item.product.name}</td>
+                  <td>{dayjs(item.createdAt).format("DD/MM/YYYY HH:MM" )}</td>
+                  <td>{item.qty}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center">
+                  ไม่พบข้อมูล
+                </td>
+              </tr>
+            )}
+          </tbody>
+          <tfoot className="text-center">
+            <tr>
+              <td colSpan="3" className="fw-bold">
+                ทั้งหมด
+              </td>
+              <td>
+                {currentStock && currentStock[`${modalType}Details`] && currentStock[`${modalType}Details`].length > 0
+                  ? currentStock[`${modalType}Details`]
+                      .reduce((acc, item) => acc + Number(item.qty), 0)
+                      .toLocaleString("th-TH")
+                  : 0}
+              </td>
+            </tr>
+          </tfoot>
         </table>
       </Modal>
     </>
